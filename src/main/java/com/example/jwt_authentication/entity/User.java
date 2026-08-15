a1731ac550c7a9fff5645379c7505ba34b52d892
Nandurkar-Prem <nandurkarprem11@gmail.com>
2026-08-15 22:24:08 +0530

    Added imp info using comments

--- src/main/java/com/example/jwt_authentication/entity/User.java
+++ src/main/java/com/example/jwt_authentication/entity/User.java
@@ -8,17 +8,23 @@ import lombok.Setter;
 @Table(name = "users")
 @Getter
 @Setter
-public class User {
+public class User {//This is a first step for any project in Spring Boot to map the entities first
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    //Here {nullable = false} means in the DB column the field email should not contain null
+    //also {unique = true} means no 2 rows have the same value for this email field.
+    //Basically email of every person should be unique.
     @Column(nullable = false, unique = true)
     private String username;
 
     @Column(nullable = false, unique = true)
     private String email;
 
+    //Here we are only using {nullable = false} because we want the field not to be empty
+    //But the password of 2 or more people can be same and the reason we are not using {unique = true}
+    //because when we hash the password the hash of same password can be different.
     @Column(nullable = false)
     private String password;
 
