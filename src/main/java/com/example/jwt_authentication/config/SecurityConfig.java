bf3671fcd90d1376678a9037dc056ff760980682
Nandurkar-Prem <nandurkarprem11@gmail.com>
2026-08-16 00:15:18 +0530

    Added protected endpoint and Authenticated into it by JWT

--- src/main/java/com/example/jwt_authentication/config/SecurityConfig.java
+++ src/main/java/com/example/jwt_authentication/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.jwt_authentication.config;
 
+import com.example.jwt_authentication.security.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,9 +9,16 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter) {
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+    }
 
     @Bean//We use this same bean in every authentication system to disable CSRF and to configure public endpoints
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
@@ -19,6 +27,10 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
                         .anyRequest().authenticated()
+                )
+                .addFilterBefore(
+                        jwtAuthenticationFilter,
+                        UsernamePasswordAuthenticationFilter.class
                 );
 
         return http.build();
--- src/main/java/com/example/jwt_authentication/security/JwtService.java
+++ src/main/java/com/example/jwt_authentication/security/JwtService.java
@@ -3,6 +3,7 @@ package com.example.jwt_authentication.security;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
@@ -30,5 +31,39 @@ public class JwtService {
                 .expiration(new Date(System.currentTimeMillis() + expiration))//Here we are setting the expiration time
                 .signWith(key)//This method signs the token using our secret key.
                 .compact();//This method converts everything into a familiar JWT Token {HEADER.PAYLOAD.SIGNATURE}
+    }
+
+    public String extractUsername(String token) {
+        SecretKey key = Keys.hmacShaKeyFor(
+                secret.getBytes(StandardCharsets.UTF_8)
+        );
+
+        return Jwts.parser()
+                .verifyWith(key)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        String username = extractUsername(token);
+        return username.equals(userDetails.getUsername())
+                && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        SecretKey key = Keys.hmacShaKeyFor(
+                secret.getBytes(StandardCharsets.UTF_8)
+        );
+        Date expiration = Jwts.parser()
+                .verifyWith(key)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getExpiration();
+
+        return expiration.before(new Date());
     }
 }
